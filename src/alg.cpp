826f89fa909b9578871a2bfe3c4626f22b63c1e1
Daniil Moiseev <33635935+daniilmoiseev@users.noreply.github.com>
2021-03-15 15:11:41 +0300

    1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-	int count = 0;
-	int r = size - 1, l = 0, m = 0;
-	while (l < r) {
-		m = (r + l) / 2;
-		if (arr[m] < value) l = m + 1;
-		else r = m;
-	}
-
-	if (arr[l] == value) {
-		while (arr[l] == value) {
-			count++; l++;
-		}
-	}
-
-	if (count != 0) return count;
-	return 0;
+  int count = 0;
+  int r = size - 1, l = 0, m = 0;
+  while (l < r) {
+    m = (r + l) / 2;
+    if (arr[m] < value) l = m + 1;
+    else r = m;
+  }
+  if (arr[l] == value) {
+    while (arr[l] == value) {
+      count++; l++;
+    }
+  }
+  if (count != 0) return count;
+  return 0;
 }
