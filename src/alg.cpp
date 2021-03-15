297ab80fab67b960e39a91e825aa98aeb9995a59
Daniil Moiseev <33635935+daniilmoiseev@users.noreply.github.com>
2021-03-15 15:37:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,23 +2,23 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int r = size - 1, l = 0, count = 0;
-	while (l < r) {
-		int m = (r + l) / 2;
-		if (arr[m] < value)
-			l = m + 1;
-		else
-			r = m;
-	}
+    while (l < r) {
+        int m = (r + l) / 2;
+        if (arr[m] < value)
+            l = m + 1;
+        else
+	    r = m;
+    }
 
-	if (arr[l] == value) {
-		while (arr[l] == value) {
-			count++; 
-			l++;
-		}
-	}
+    if (arr[l] == value) {
+        while (arr[l] == value) {
+            count++; 
+	    l++;
+        }
+    }
 
-	if (count)
-		return count;
-	else
-		return 0;
+    if (count)
+        return count;
+    else
+        return 0;
 }
